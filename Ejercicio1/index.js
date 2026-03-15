6f00a74f09f13765a1abc991a053b6fb3d151450
alegp2020 <alexpicos644@gmail.com>
2026-03-15 13:47:59 +0100

    corregido

--- Ejercicio1/index.js
+++ Ejercicio1/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const app = express;
+const app = express();
 
-app.length("/",(req,res)=> {
-    res.console("Ejecutando codigo de mi primer script")
+app.get("/",(req,res)=> {
+    res.send("Ejecutando codigo de mi primer script")
 })
+
 app.listen(3000, () => {
     console.log("Servidor escuchando en el puerto 3000");
 });
